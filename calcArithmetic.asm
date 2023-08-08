1e8c2aaadb5cddd80643f6261bafdb4acf1dd564
Danh Phan <phandanh2112@gmail.com>
2023-08-08 12:25:54 +0700

    calcArithmetic - ver 2.0 - sum, sub (1->4 digit) operands

--- calcArithmetic.asm
+++ calcArithmetic.asm
@@ -1,14 +1,11 @@
-.model small
-                                           
+.model small                                                 
 .data 
-    msg1 db 10,13, "Enter the first number: $"             ; 10,13 -> /n
+    msg1 db 10,13, "Enter the first number: $"   ; 10,13 -> /n
     msg2 db 10,13, "Enter the second number: $"  
     msg3 db 10,13, "The sum of 2 numbers: $"
-    msg4 db 10,13, "The different of 2 number: $" 
+    msg4 db 10,13, "The different of 2 number: $"
     msg5 db "-$"
-    msg6 db 10,13, "The product of 2 numbers: $" 
-    msg7 db 10,13, "The quotient of 2 numbers: $"
-    msg8 db 10,13, "The remainder of division: $" 
+    msg6 db 10,13, "Enter the number of digits: $" 
 
 
 
@@ -16,158 +13,165 @@
 main proc
     mov ax, @data 
     mov ds, ax   
-    mov bp, 10  
+    mov bp, 10         ; const
     mov cx, 0
-          
-          
-          
-InputNum1: 
-    mov ah, 9           ; show msg
-    lea dx, msg1       
-    int 21h   
-               
-    mov ah, 1           ; save input <hex> into al reg
-    int 21h            
-          
-    cmp al, 30h         ; enter input again if input is not in range of 0-9
-    jb InputNum1    
-    cmp al, 39h          
-    ja InputNum1  
-           
-    sub al, 30h         ; save num1 to calculate
-    mov ah, 0            
-    push ax                 
-          
-          
-          
-InputNum2:
-    mov ah,9            ; show msg
-    lea dx, msg2         
-    int 21h     
-           
-    mov ah, 1           ; save input <hex> into al reg
-    int 21h       
-            
-    cmp al, 30h         ; enter input again if input is not in range of 0-9 
-    jb InputNum2     
-    cmp al, 39h          
-    ja InputNum2  
-           
-    sub al, 30h         ; save num2 to calculate
-    mov ah, 0            
-    push ax                  
-          
-          
-          
-Addition:          
-    mov ah, 9           ; show msg
-    lea dx, msg3        
-    int 21h        
-       
-    call getOperands            
-    add ax, bx          ; add  
-          
-    call handleResult   
-           
-           
-           
-Subtraction:          
-    mov ah, 9           ; show msg
-    lea dx, msg4        
-    int 21h             
-    
-    call getOperands      
-    cmp ax, bx          ; compare 2 numbers
-    jb signed           ; num1 < num2
-    jnb unsigned        ; num1 >= num2  
-        
-unsigned:    
-    sub ax, bx          ; sub   
-            
-    call handleResult        
-    jmp Multiplication   
-    
-signed: 
-    mov ah, 9           ; show msg
-    lea dx, msg5        
-    int 21h      
-    
-    call getOperands    
-    sub bx, ax          ; sub
-    mov ax, bx 
-             
-    call handleResult 
-      
-      
-        
-Multiplication:    
+
+
+
+Input_numsDig:
     mov ah, 9           ; show msg
     lea dx, msg6       
-    int 21h       
-    
-    call getOperands    ; mul   
-    mul bl              
-            
-    call handleResult                 
-      
-      
-      
-Division:
-    mov ah, 9           ; show msg
-    lea dx, msg7       
     int 21h                
     
-    call getOperands    ; div
-    div bl              ; quotient
-    mov bl, ah          ; remainder 
+    mov ah, 1           ; input value
+    int 21h
+      
+    cmp al, 30h         ; enter input again if input is not in range of 0-9
+    jb Input_numsDig        
+    cmp al, 39h
+    ja Input_numsDig         
+    
+    cmp al, 30h         ; compare to classify
+    je False        
+    cmp al, 34h
+    jle True    
         
-    call handleResult        
-                     
+False:    
+    mov dl, 'F'         
+    mov ah, 2     
+    int 21h            
+    jmp Input_numsDig     
+                        
+True:
+    mov ah, 0           
+    sub al, 30h
+    mov si, ax          ; number of digits         
+    mov dl, 'T'          
+    mov ah, 2    
+    int 21h  
+          
+          
+          
+Input_Num1:
     mov ah, 9           ; show msg
-    lea dx, msg8       
-    int 21h                
+    lea dx, msg1       
+    int 21h              
     
-    mov ax, bx          ; get remainder
+    mov cx, si          ; create the number of loops
+    call handleInput        
+    
+    mov di, 0           ; reset
+    push ax             ; store num1
+         
+         
+         
+Input_Num2:
+    mov ah, 9           ; show msg
+    lea dx, msg2       
+    int 21h        
+    
+    mov cx, si          ; create the number of loops
+    call handleInput    
+    
+    push ax             ; store num2 
+        
+        
+        
+Summation:          
+    mov ah, 9           ; show msg
+    lea dx, msg3        
+    int 21h         
+      
+    call getOperands     
+  
+    add ax, bx          ; add 
+              
     call handleResult
        
        
        
+Subtraction:          
+    mov ah, 9           ; show msg
+    lea dx, msg4        
+    int 21h          
+    
+    call getOperands      
+    
+    cmp ax, bx          ; compare 2 numbers
+    jb signed           ; num1 < num2
+    jnb unsigned        ; num1 >= num2  
+            
+unsigned:    
+    sub ax, bx          ; sub           
+    call handleResult
+    jmp Exit
+     
+signed: 
+    mov ah, 9           ; show msg
+    lea dx, msg5        
+    int 21h     
+    call getOperands    
+    sub bx, ax          ; sub
+    mov ax, bx          
+    call handleResult   
+   
+   
+    
 Exit:
     mov ah, 0
-    int 21h     
-   
-   
-   
-getOperands:
-    pop si              ; address
-    pop bx              ; num2
-    pop ax              ; num1 
-      
-    push ax             ; save num1 into stack to calculate 
-    push bx             ; save num2 into stack to calculate
-    push si             ; save address to return previous block code
+    int 21h 
+ 
+ 
+ 
+x10:    
+    mul bp          
+    mov di, ax  
+         
+handleInput:   
+    mov ah, 1             
+    int 21h   
+    
+    sub al, 30h 
+    mov ah, 0  
+           
+    add ax, di
+    loop x10              
     ret
+      
+      
+      
+getOperands:
+    pop si              ; address of previous block code
+    pop bx              ; num2
+    pop ax              ; num1       
     
+    push ax             ; store num1 again to calculate 
+    push bx             ; store num2 again to calculate
+    push si             ; store address to return previous block code   
     
+    ret
        
+       
+                        
 handleResult: 
     inc cl              ; create the number of loops 
     
-    mov ah, 0           ; number separation
     mov dx, 0              
     div bp
-    push dx             ; save number into stack to show result  
+    push dx             ; store remainder to show result  
        
-    cmp ax, 0           ; check done processing ?
+    cmp ax, 0
     jne handleResult
-    
-    
-    
+           
+           
+           
 showResult:             
-    pop dx              ; get number to display
-    add dl, 30h        
+    pop dx
+    add dl, 30h
+        
+    mov ah, 2
+    int 21h               
     
-    mov ah, 2           ; display result
-    int 21h      
-    loop showResult            
-    ret                 
+    loop showResult    
+            
+    ret  
